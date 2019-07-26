ae72bc899047bc19d386bc7dde44b8af898129ea
ALY RHIDO FAZUANI <53285416+Deku470@users.noreply.github.com>
2019-07-26 09:31:17 +0700

    Add files via upload

---
+++ temensaya.php.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+
+
+ ?>
+
+<!doctype html>
+<html>
+<head>
+	<style type="text/css">
+		body {
+			background: url(AncientJampackedBlacknorwegianelkhound-size_restricted.gif);
+			background-size: 100% ;
+		}
+		.badan {
+			font-size: 24px;
+			color: salmon;
+		}
+	</style>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body class="">
+<center>
+<img opacity="50%" weidht="250px" height="250px" src="./blblblb.png" alt="Foto temen saya"/>
+<div class="badan">				
+<h1 style="color: Tomato;">TEMEN SAYA</h1>
+	
+ <p>Dijual Teman GakGuna Pas Main PUBG. Gakh bawah medhkith</p>
+ <br><br><br><br><br><hr>
+ <p>Andrew Darwis (lahir 20 Juli 1979; umur 36 tahun) beliau 
+            adalah pendiri komunitas daring terbesar di Indonesia, Kaskus. 
+            Andrew sekarang menjabat sebagai Chief Technology Officer (CTO) PT Darta Media Indonesia (Kaskus) 
+            sekaligus pemilik (owner) Kaskus Network lewat PT Darta Media Indonesia. 
+            Andrew mendirikan Kaskus pada 6 November 1999. 
+            Bermula dari pengalamannya saat menimba ilmu di universitas Seattle University di Amerika, 
+            Program Studi Multimedia & Web Design, 
+            Art Institute of Seattle Computer Science pada tahun 1999, 
+            pria yang disapa Andrew ini terinspirasi membuat website forum komunitas 
+            yang bisa di bilang menjadi yang terbesar di Indonesia. 
+            Saat itu saya ditugaskan oleh dosen untuk membuat program dari free software, 
+            dari situlah mulai muncul ide membuat website dengan nama Kaskus.	</p>
+  </div>
+  </center>
+
+  <footer style="color : white ; text-align: center; font-size: 27px; margin-bottom: 10px;">Copyright Robot</footer>
+
+</body>
+</html>
